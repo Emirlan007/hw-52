aed9a52bd4c41b16a569cac2c7f997094d6bc2a4
Emirlan Kaldarbekov <kaldarbekovemirlan630@gmail.com>
2024-12-24 16:52:59 +0600

    Добавлена функциональность раздачи карт и отображение выбранных карт на экране.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
-import './App.css'
+import './App.css';
+import CardDeck from "./lib/CardDeck.ts";
+import Card from "./lib/Card.ts";
+import { useState } from 'react';
 
+const App = () => {
+    const [cards, setCards] = useState<Card[]>([]);
 
-const App = () => (
-    <>a
+    const handleDealCards = () => {
+        const deck = new CardDeck();
+        if (deck.cards.length < 5) {
+            alert('Не хватает карт в колоде!');
+        } else {
+            const dealtCards = deck.getCards(5);
+            setCards(dealtCards);
+        }
+    };
 
-    </>
-);
+    return (
+        <>
+            {cards.length > 0 && (
+                <div className="playingCards faceImages">
+                    {cards.map((card, index) => (
+                        <span key={index} className={`card rank-${card.rank} ${card.suit}`}>
+                            <span className="rank">{card.rank}</span>
+                            <span className="suit">{}</span>
+                        </span>
+                    ))}
+                </div>
+            )}
+            <button onClick={handleDealCards}>Раздать карты</button>
+        </>
+    );
+};
 
-export default App
+export default App;
